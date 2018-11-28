02b62b49c5278fb98c669907ec1362d6c2c9f143
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-11-27 23:24:46 -0600

    Add maint files

--- lib/Renard/Incunabula/Source/DICT.pm
+++ lib/Renard/Incunabula/Source/DICT.pm
@@ -5,3 +5,10 @@ use strict;
 use warnings;
 
 1;
+__END__
+
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Incunabula-Source-DICT/>
+
+=cut
